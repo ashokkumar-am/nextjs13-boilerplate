f2ba3a3fa9400eaebdfe9d3513501b480e6a7726
Ashok Kumar <4979471+ashokkumar-am@users.noreply.github.com>
2023-07-02 23:56:01 +0530

    few more about us page added

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -1,3 +1,5 @@
+import AboutTailwind from "@/components/common/About/About-Tailwind";
+import About1Tailwind from "@/components/common/About/About1-Tailwind";
 import ClientComponent from "@/components/common/Clients/ClientComponent";
 import CustomImage from "@/components/common/CustomImage/CustomImageComponent";
 import Hero from "@/components/common/Hero/Hero";
@@ -11,18 +13,10 @@ const page = () => {
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   };
   return (
-    <div className="h-80 min-h-screen">
-      <div className="hero h-80">
-        <h1 className="green_gradient text-5xl">Welcome to About Page</h1>
-        <h1 className="blue_purple_pink text-4xl ">SARAH DEEKSHA </h1>
-        <h1 className="indigo_sky_green text-2xl">ASHOK KUMAR A M</h1>
-        
-      </div>
-      <Hero 
-        title="Welcome to My Hero App" image={"/banner2.jpeg"}
-      />
-      <ClientComponent client={clientData} />
-    </div>
+   <>
+   {/* <About1Tailwind/> */}
+   <AboutTailwind/>
+   </>
   );
 };
 
